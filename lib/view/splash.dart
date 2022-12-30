f83d4f7a23ecb35625e19085a6236091499c32ba
paratonsp <paratonsp@gmail.com>
2022-12-30 09:36:20 +0700

    notification

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:overlay_support/overlay_support.dart';
 import 'package:revver/controller/auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
