eb3268baf02ffcf2899c5bea4dbc793f382bcc67
paratonsp <paratonsp@gmail.com>
2023-01-11 21:53:54 +0700

    notification

--- lib/view/notifications/notifications.dart
+++ lib/view/notifications/notifications.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:revver/component/spacer.dart';
+import 'package:revver/controller/event.dart';
 import 'package:revver/globals.dart';
+import 'package:revver/model/event.dart';
 
 class Notifications extends StatefulWidget {
   const Notifications({Key key}) : super(key: key);
@@ -12,6 +14,25 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
+  bool isLoad = true;
+  List<Event> event = [];
+
+  getData() async {
+    await getEvent().then((val) {
+      if (!mounted) return;
+      setState(() {
+        event = val;
+        isLoad = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,7 +73,7 @@ class _NotificationsState extends State<Notifications> {
               SpacerHeight(h: 40),
               Expanded(
                 child: Container(
-                  padding: EdgeInsets.only(top: 40, left: 20, right: 20),
+                  padding: EdgeInsets.only(top: 55, left: 35, right: 35),
                   decoration: BoxDecoration(
                     color: CustomColor.backgroundColor,
                     borderRadius: BorderRadius.only(
@@ -60,78 +81,87 @@ class _NotificationsState extends State<Notifications> {
                       topRight: Radius.circular(65),
                     ),
                   ),
-                  child: ListView.builder(
-                    physics: BouncingScrollPhysics(),
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    itemCount: 20,
-                    itemBuilder: ((context, index) {
-                      // Member mem = member[index];
-                      // String id = mem.id.toString();
-                      return Column(
-                        children: [
-                          InkWell(
-                            // onTap: () => GoRouter.of(context)
-                            //     .push('/downline-detail/$id'),
-                            child: Container(
-                              height: 70,
-                              decoration: BoxDecoration(
-                                color: CustomColor.whiteColor,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.1),
-                                    spreadRadius: 0,
-                                    blurRadius: 13,
-                                    offset: Offset(0, 3),
-                                  ),
-                                ],
-                              ),
-                              child: Row(
-                                children: [
-                                  SpacerWidth(w: 10),
-                                  Container(
-                                    height: 50,
-                                    width: 50,
+                  child: (isLoad)
+                      ? Center(
+                          child: CupertinoActivityIndicator(),
+                        )
+                      : ListView.builder(
+                          physics: BouncingScrollPhysics(),
+                          padding: EdgeInsets.zero,
+                          shrinkWrap: true,
+                          itemCount: event.length,
+                          itemBuilder: ((context, index) {
+                            Event ev = event[index];
+                            String id = ev.id.toString();
+                            return Column(
+                              children: [
+                                InkWell(
+                                  onTap: () => GoRouter.of(context)
+                                      .push("/global-event/$id"),
+                                  child: Container(
+                                    height: 70,
                                     decoration: BoxDecoration(
-                                      color: CustomColor.greyColor,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: Icon(
-                                      Icons.notifications,
                                       color: CustomColor.whiteColor,
+                                      borderRadius: BorderRadius.circular(10),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.1),
+                                          spreadRadius: 0,
+                                          blurRadius: 13,
+                                          offset: Offset(0, 3),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        SpacerWidth(w: 10),
+                                        Container(
+                                          height: 50,
+                                          width: 50,
+                                          decoration: BoxDecoration(
+                                            color: CustomColor.greyColor,
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                          child: Icon(
+                                            Icons.notifications,
+                                            color: CustomColor.whiteColor,
+                                          ),
+                                        ),
+                                        SpacerWidth(w: 10),
+                                        Expanded(
+                                          child: Text(
+                                            ev.name,
+                                            style: CustomFont(
+                                                    CustomColor.blackColor,
+                                                    18,
+                                                    FontWeight.w600)
+                                                .font,
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 2,
+                                          ),
+                                        ),
+                                        SpacerWidth(w: 10),
+                                        Text(
+                                          ev.date,
+                                          style: CustomFont(
+                                                  CustomColor.oldGreyColor,
+                                                  14,
+                                                  FontWeight.w600)
+                                              .font,
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 2,
+                                        ),
+                                        SpacerWidth(w: 10),
+                                      ],
                                     ),
                                   ),
-                                  SpacerWidth(w: 10),
-                                  Expanded(
-                                    child: Text(
-                                      "mem.name",
-                                      style: CustomFont(CustomColor.blackColor,
-                                              18, FontWeight.w600)
-                                          .font,
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 2,
-                                    ),
-                                  ),
-                                  SpacerWidth(w: 10),
-                                  Text(
-                                    "mem.stage_name",
-                                    style: CustomFont(CustomColor.oldGreyColor,
-                                            14, FontWeight.w600)
-                                        .font,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                  ),
-                                  SpacerWidth(w: 10),
-                                ],
-                              ),
-                            ),
-                          ),
-                          SpacerHeight(h: 10),
-                        ],
-                      );
-                    }),
-                  ),
+                                ),
+                                SpacerHeight(h: 10),
+                              ],
+                            );
+                          }),
+                        ),
                 ),
               ),
             ],
