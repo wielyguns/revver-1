3b4977714a94f9b17315f8d5fb5dfb2c4357863a
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2023-03-31 02:47:40 +0700

    updatePlan

--- lib/view/plan/plan.dart
+++ lib/view/plan/plan.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: must_be_immutable
+
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -28,7 +32,7 @@ class _PlanState extends State<Plan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: CustomColor.brownColor,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -36,86 +40,111 @@ class _PlanState extends State<Plan> {
             color: CustomColor.whiteColor,
             onPressed: () => GoRouter.of(context).pop()),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      spreadRadius: 3,
-                      blurRadius: 10,
-                    ),
-                  ],
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: SfPdfViewer.asset(
-                    "assets/pdf/SLIDE-MARKETING-PLAN-POTRAIT.pdf",
-                    controller: _pdfViewerController,
-                    canShowScrollHead: false,
-                    scrollDirection: PdfScrollDirection.horizontal,
-                    pageLayoutMode: PdfPageLayoutMode.single,
-                  ),
-                ),
+      body: Stack(
+        children: [
+          Container(
+            height: CustomScreen(context).height,
+            width: CustomScreen(context).width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/img/BG-POTRAIT.jpg"),
+                fit: BoxFit.cover,
               ),
             ),
-            SpacerHeight(h: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 0, left: 25, right: 25, bottom: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                GestureDetector(
-                  onTap: (() {
-                    _pdfViewerController.previousPage();
-                    setState(() {});
-                  }),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 5, left: 5, bottom: 5),
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.black,
+                AspectRatio(
+                  aspectRatio: 9 / 16,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: SfPdfViewer.asset(
+                        "assets/pdf/SLIDE-MARKETING-PLAN-POTRAIT-20230330.pdf",
+                        controller: _pdfViewerController,
+                        canShowScrollHead: false,
+                        onPageChanged: (x) {
+                          setState(() {});
+                        },
+                        onDocumentLoaded: (x) {
+                          setState(() {});
+                        },
+                        scrollDirection: PdfScrollDirection.horizontal,
+                        pageLayoutMode: PdfPageLayoutMode.single,
+                      ),
                     ),
                   ),
                 ),
-                Text(_pdfViewerController.pageNumber.toString()),
-                GestureDetector(
-                  onTap: (() {
-                    _pdfViewerController.nextPage();
-                    setState(() {});
-                  }),
-                  child: Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.black,
+                SpacerHeight(h: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: (() {
+                        _pdfViewerController.previousPage();
+                        setState(() {});
+                      }),
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 5, left: 5, bottom: 5),
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                SpacerWidth(w: 10),
-                GestureDetector(
-                  onTap: (() {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => PlanLandscapeMode()),
-                    );
-                  }),
-                  child: Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Icon(
-                      Icons.fullscreen,
-                      color: Colors.black,
+                    Text(_pdfViewerController.pageNumber.toString()),
+                    GestureDetector(
+                      onTap: (() {
+                        _pdfViewerController.nextPage();
+                        setState(() {});
+                      }),
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                    SpacerWidth(w: 10),
+                    GestureDetector(
+                      onTap: (() {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => GoToLandScape(
+                              goToLandscape: true,
+                            ),
+                          ),
+                        );
+                      }),
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.fullscreen,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -153,11 +182,70 @@ class _PlanLandscapeModeState extends State<PlanLandscapeMode> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: SfPdfViewer.asset(
-        "assets/pdf/SLIDE-MARKETING-PLAN-LANDSCAPE.pdf",
-        scrollDirection: PdfScrollDirection.horizontal,
-        pageLayoutMode: PdfPageLayoutMode.single,
+      body: Stack(
+        children: [
+          Container(
+            height: CustomScreen(context).height,
+            width: CustomScreen(context).width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/img/BG LANDSCAPE.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 36),
+            child: AspectRatio(
+              aspectRatio: 23.2 / 9,
+              child: SfPdfViewer.asset(
+                "assets/pdf/SLIDE-MARKETING-PLAN-LANDSCAPE-20230330.pdf",
+                scrollDirection: PdfScrollDirection.horizontal,
+                pageLayoutMode: PdfPageLayoutMode.single,
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class GoToLandScape extends StatefulWidget {
+  GoToLandScape({Key key, this.goToLandscape}) : super(key: key);
+  bool goToLandscape;
+
+  @override
+  State<GoToLandScape> createState() => _GoToLandScapeState();
+}
+
+class _GoToLandScapeState extends State<GoToLandScape> {
+  @override
+  void initState() {
+    Timer(
+      Duration(milliseconds: 500),
+      (() {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => PlanLandscapeMode()),
+        ).whenComplete(() {
+          Timer(
+            Duration(milliseconds: 1000),
+            (() {
+              Navigator.pop(context);
+            }),
+          );
+        });
+      }),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColor.backgroundColor,
+      body: Center(child: CupertinoActivityIndicator()),
     );
   }
 }
